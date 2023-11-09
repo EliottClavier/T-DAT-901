5e9ecc59132c4df45857128ad6b67137a8a98ba9
Clavier Eliott <eliott.clavier@epitech.eu>
2023-11-09 22:15:35 +0100

    [DAT-INFLUX] Set connection between spark and influxdb bucket

--- spark/apps/main.py
+++ spark/apps/main.py
@@ -1,8 +1,10 @@
+import datetime
 import os
 import logging
 
 from pyspark.sql import SparkSession
-
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 # Configuration de logging pour styliser la sortie
 logger = logging.getLogger("kafka-messages")
@@ -11,33 +13,79 @@ handler.setFormatter(logging.Formatter('%(message)s'))
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
+class SparkStreaming:
 
-def stylized_log(message):
-    logger.info("===" * 10)
-    logger.info(message)
-    logger.info("===" * 10)
+    # Spark
+    spark: SparkSession = None
+    raw_stream = None
+    string_stream = None
 
+    # InfluxDB
+    client: InfluxDBClient = None
+    measurement = "CryptoCurrency"
+
+    def __init__(self):
+
+        # Initialize Spark
+        self.spark = SparkSession.builder \
+            .appName("KafkaPySparkStreaming") \
+            .getOrCreate()
+
+        self.raw_stream = self.spark.readStream \
+            .format("kafka") \
+            .option("kafka.bootstrap.servers", os.environ["KAFKA_BOOTSTRAP_SERVERS"]) \
+            .option("subscribe", os.environ["KAFKA_DEFAULT_TOPIC"]) \
+            .load()
+
+        self.string_stream = self.raw_stream.selectExpr("CAST(value AS STRING) as value")
+
+    def write_to_influxdb(self, pandas_df):
+
+        client = InfluxDBClient(
+            url=f"http://{os.environ['INFLUXDB_HOST']}:{os.environ['INFLUXDB_PORT']}",
+            token=os.environ["INFLUXDB_ADMIN_TOKEN"],
+            org=os.environ["INFLUXDB_ORG"]
+        )
+
+        write_api = client.write_api(write_options=SYNCHRONOUS)
+
+        for index, row in pandas_df.iterrows():
+
+            # String to timestamp
+            row["TimeStamp"] = datetime.datetime.fromtimestamp(int(row["TimeStamp"]))
+
+            point = (Point(self.measurement)
+                     .time(row["TimeStamp"])
+                     .tag("CurrencyName", row["CurrencyName"])
+                     .field("CurrencyPair", row["CurrencyPair"])
+                     .field("CurrencySymbol", row["CurrencySymbol"])
+                     .field("ExchangeName", row["ExchangeName"])
+                     .field("Price", row["Price"])
+                     .field("Volume24H", row["Volume24H"])
+                     .field("CirculatingSupply", row["CirculatingSupply"])
+                     .field("Liquidity", row["Liquidity"])
+                 )
+
+            write_api.write(bucket=os.environ["INFLUXDB_BUCKET"], org=os.environ["INFLUXDB_ORG"], record=point)
 
 def main():
-    spark = SparkSession.builder \
-        .appName("KafkaPySparkStreaming") \
-        .getOrCreate()
+    spark_streaming = SparkStreaming()
 
-    raw_stream = spark.readStream \
-        .format("kafka") \
-        .option("kafka.bootstrap.servers", os.environ["KAFKA_BOOTSTRAP_SERVERS"]) \
-        .option("subscribe", os.environ["KAFKA_DEFAULT_TOPIC"]) \
-        .load()
-
-    string_stream = raw_stream.selectExpr("CAST(value AS STRING) as value")
-
-    # À chaque fois qu'une donnée est reçue, la log avec un style personnalisé
     def foreach_batch_function(df, epoch_id):
-        messages = df.collect()
-        for row in messages:
-            stylized_log(row["value"])
+        # Transform value into df with all columns in string json of value
+        pandas_df = df.withColumn("value", df["value"].cast("string"))
+        pandas_df = pandas_df.selectExpr("CAST(value AS STRING) as value")
+        pandas_df = pandas_df.selectExpr("from_json(value, 'CurrencyName STRING, CurrencyPair STRING, CurrencySymbol STRING, ExchangeName STRING, Price STRING, Volume24H STRING, CirculatingSupply STRING, Liquidity STRING, TimeStamp STRING') AS value")
+        pandas_df = pandas_df.select("value.*")
 
-    query = string_stream.writeStream.foreachBatch(foreach_batch_function).outputMode("append").start()
+        # To pandas df
+        pandas_df = pandas_df.toPandas()
+
+        # Write to InfluxDB
+        spark_streaming.write_to_influxdb(pandas_df)
+
+
+    query = spark_streaming.string_stream.writeStream.foreachBatch(foreach_batch_function).outputMode("append").start()
 
     query.awaitTermination()
 
