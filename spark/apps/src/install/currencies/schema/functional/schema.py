3ea856177953681459763dc2b1b6d4f3ac9fbfd4
Xamimus <pro.maximemartin@gmail.com>
2024-01-25 10:04:20 +0100

    [DAT-SPARK] Add currencies volume 24h field

--- spark/apps/src/install/currencies/schema/functional/schema.py
+++ spark/apps/src/install/currencies/schema/functional/schema.py
@@ -3,7 +3,8 @@ from pyspark.sql.types import StructType, StructField, StringType, DoubleType, L
 functional_schema = StructType([
     StructField("CurrencyName", StringType(), False),
     StructField("ExchangeName", StringType(), False),
-    StructField("Price", DoubleType(), True),
+    StructField("Price", DoubleType(), False),
+    StructField("Volume24H", LongType(), False),
     StructField("dhi", LongType(), False),
     StructField("dht", LongType(), False)
 ])
